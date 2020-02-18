2ced4280dc6555d6b9edbd6ae6cc9a31adf780f4
FHanus <filiphanus1@gmail.com>
2020-02-18 18:34:49 +0100

    Update webFunctions.h

--- Code/webFunctions.h
+++ Code/webFunctions.h
@@ -6,12 +6,20 @@ void handleRoot() {
 }
 // Obstarávání ajax kódu pro načítání do tabulky, načtení všech souřadnic do jednoho stringu
 void handlevalues() {
-  message = (String(strd[0][0]) + "," + String(strd[0][1])); 
-  for(int i=1; i<13;i++){
+  if(PovMereni==1){message = "";}
+  if(PovMereni==0){message = "Nastavte chladič do měřitelné polohy\na obnovte měření!";}
+  for(int i=0; i<13;i++){
     for(int d=0; d<2;d++){
       if((strd[i][d])!=0){
         mest = String(strd[i][d]); 
-        mest.remove(3, 2);
+        Serial.println(strd[i][d]);
+        if(mest.length() == 5){mest.remove(3);}
+        if(mest.length() == 4){mest.remove(2);}
+        if(mest.length() == 3){mest.remove(1);}
+        
+        if ((mest.length() > 5)or(mest == "-10000")) {
+          mest = "Mimo dosah";        
+        }
       }
       else{
         mest = ("N/A"); 
@@ -26,12 +34,34 @@ void handleLED() {
  String led0State = "OFF";
  String led1State = "OFF";
 
+
  // Odkazuje na xhttp.open("GET", "setLED?LEDstate="+led, true); v index.h
  String t_state = server.arg("LEDstate"); 
+
  // START
- if(t_state == "0"){
+ if((t_state == "0")&&(oldt_state != "0")){
     led0State = "ON";
     led1State = "OFF";
+    KrokMan = "S";
+
+    if(PovMereni==0){
+      strd[0][0]=0;
+      strd[0][1]=0;
+      phase2 = 0;
+      poradnik = 0;
+      merX[0] = 0;   
+      merX[1] = 0;               
+      merY[0] = 0;   
+      merY[1] = 0;   
+      lastPos[0] = 0;   
+      lastPos[1] = 0;   
+      lastPosWD[0] = 0;   
+      lastPosWD[1] = 0;   
+      mathValX = 0;
+      mathValY = 0;
+      PovMereni = 1;
+    }
+
     valG = 1;
     valB = 0;
     digitalWrite(ledPinG, valG);
@@ -41,18 +71,16 @@ void handleLED() {
     runallowed = true;
  }
  // PAUSE
- if(t_state == "1"){
+ if((t_state == "1")&&(oldt_state != "1")){
     led0State = "OFF";
     led1State = "ON";
     valG = 0;
     valB = 1; 
 
-
     RunToLastPos=1; 
 
     oldPos[0]=Xaxis.currentPosition();
     oldPos[1]=Yaxis1.currentPosition();
-
 
     runM=0;
     poradnikMan=1;
@@ -61,25 +89,31 @@ void handleLED() {
     runallowed = false;  
  }
  // Jednotlivé směry při manuálu
- if(t_state == "2"){
-   KrokMan = "Yp";
- }
- if(t_state == "3"){
-    KrokMan = "Xm";
- }
- if(t_state == "4"){
-   KrokMan = "S";
- }
- if(t_state == "5"){
-    KrokMan = "Xp";
- }
- if(t_state == "6"){
-    KrokMan = "Ym";
+ if(runallowed == false){
+  if(t_state == "2"){
+    KrokMan = "Yp";
+  }
+  if(t_state == "3"){
+      KrokMan = "Xm";
+  }
+  if(t_state == "4"){
+    KrokMan = "S";
+  }
+  if(t_state == "5"){
+      KrokMan = "Xp";
+  }
+  if(t_state == "6"){
+      KrokMan = "Ym";
+  }
  }
 
  // Pošle webovou stránku serveru
  server.send(200, "text/plane", led0State);
- server.send(200, "text/plane", led1State);  
+ server.send(200, "text/plane", led1State); 
+
+ if((t_state=="1")or(t_state=="0")){
+   oldt_state = t_state;
+ }
 }
 // Obstarávání formu pro načítání požadovaných hodnot z html form
 void handleForm() { 
